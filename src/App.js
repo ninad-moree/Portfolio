5aeb0a67ea39204da85944380b46ac5d1001efeb
Ninad More <ninadyogeshmore@gmail.com>
2023-12-15 16:31:56 +0530

    navbar component done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import {useEffect, useState} from "react";
+import useMediaQuery from "./hooks/useMediaQuery";
+import Navbar from "./scenes/Navbar";
+import DotGroup from "./scenes/DotGroup";
+
 function App() {
+  const [selectedPage, setSelectedPage] = useState('home');
+  const [isTopOfPage, setIsTopOfPage] = useState(true); 
+  const isAboveMediumScreen = useMediaQuery("(min-width: 1060px)");
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0)
+        setIsTopOfPage(true);
+      if (window.screenY !== 0)
+        setIsTopOfPage(false);
+    }
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <div className="App">
-      
+    <div className="app bg-deep-blue">
+      <Navbar isTopOfPage={isTopOfPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+
+      <div className="w-5/6 mx-auto md:h-full">
+        {isAboveMediumScreen && (
+          <DotGroup selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+        )}
+      </div>
     </div>
   );
 }
