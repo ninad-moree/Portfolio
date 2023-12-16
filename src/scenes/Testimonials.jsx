c5ed20dddf34b300de8840b6f1f388a19da38c50
Ninad More <ninadyogeshmore@gmail.com>
2023-12-16 17:24:35 +0530

    testimonials minor change

--- src/scenes/Testimonials.jsx
+++ src/scenes/Testimonials.jsx
@@ -11,7 +11,7 @@ const Testimonials = () => {
             <motion.div className="md:w-1/3 text-center md:text-left" initial="hidden" whileInView="visible" viewport={{once: true, amount: 0.5}} transition={{duration: 0.5}} variants={{hidden: {opacity:0, x:-50}, visible:{opacity:1, x:0}}}>
                 <p className="font-playfair font-semibold text-4xl mb-5 text-red">TESTIMONIALS</p>
 
-                <LineGradient width="mx-auto w-2/5" />
+                <LineGradient width="w-2/5" />
 
                 <p className="mt-10">
                     Here's What People are Saying About My Work. Aliquam aliquet integer
