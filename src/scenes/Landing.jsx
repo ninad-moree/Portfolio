86ba5434610f063be8d045799dcc391724a835d8
Ninad More <ninadyogeshmore@gmail.com>
2023-12-16 15:22:31 +0530

    skills page done

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -27,7 +27,7 @@ const Landing = ({setSelectedPage}) => {
             {/* MAIN SECTION */}
             <div className="z-30 basis-2/5 mt-12 md:mt-32">
                 {/* HEADING */}
-                <motion.div initial="hidden" whileInView="visible" viewport={{once: true, amount: 0.5}} transition={{duration: 0.5}} variants={{hidden: {opacity:0, x:-50}, visible:{opacity:1, x:1}}}>
+                <motion.div initial="hidden" whileInView="visible" viewport={{once: true, amount: 0.5}} transition={{duration: 0.5}} variants={{hidden: {opacity:0, x:-50}, visible:{opacity:1, x:0}}}>
                     <p className="text-6xl font-playfair z-10 text-center md:text-start">
                         Jane {""}
                         <span className="xs: relative xs:text-deep-blue xs:font-semibold z-20 xs:before:content-brush before:absolute before:-left-[25px] before:-top-[70px] before:z-[-1]">
